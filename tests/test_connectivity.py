720eed39fe7d235ed9591157596334a65531a187
splitnines <splitnines@gmail.com>
2025-08-16 06:41:49 -0700

    fix: test_connectivity.py

--- tests/test_connectivity.py
+++ tests/test_connectivity.py
@@ -1,14 +1,36 @@
+# import pytest
+# from unicon.core.errors import ConnectionError
+#
+#
+# def test_connect_cli(tb, dev_name):
+#     dev = tb.devices[dev_name]
+#     try:
+#         dev.connect(log_stdout=False)
+#         dev.execute("show clock")
+#     except ConnectionError as e:
+#         pytest.fail(f"Failed to connect to {dev_name}: {e}")
+#     finally:
+#         if dev.is_connected():
+#             dev.disconnect()
 import pytest
-from unicon.core.errors import ConnectionError
+from genie.testbed import load
+from ._inventory import devices_iosxe
 
 
-def test_connect_cli(tb, dev_name):
-    dev = tb.devices[dev_name]
+@pytest.fixture(scope="session")
+def tb():
+    return load("testbed/testbed.yaml")
+
+
+@pytest.mark.parametrize("d", devices_iosxe())
+def test_mgmt_interface_up_up(tb, d):
+    dev = tb.devices[d["name"]]
+    dev.connect(log_stdout=False)
     try:
         dev.connect(log_stdout=False)
         dev.execute("show clock")
     except ConnectionError as e:
-        pytest.fail(f"Failed to connect to {dev_name}: {e}")
+        pytest.fail(f"Failed to connect to {dev}: {e}")
     finally:
         if dev.is_connected():
             dev.disconnect()
