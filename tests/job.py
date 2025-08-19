1c2a3638d5bb6721fd880620f780ff4f808f4a93
splitnines <splitnines@gmail.com>
2025-08-19 11:59:14 -0700

    TESTING

--- tests/job.py
+++ tests/job.py
@@ -1,11 +1,32 @@
-# from pyats.easypy import run
+# def main(runtime):
+#     runtime.job.name = "verify_ntp.py"
+#
+#     runtime.tasks.run(
+#         testscript="tests/verify_ntp.py",
+#         testbed="tests/testbed.yaml",
+#         runtime=runtime,
+#     )
+#
+#
+import os
+from genie.testbed import load
 
 
 def main(runtime):
-    runtime.job.name = "verify_ntp.py"
+    # ----------------
+    # Load the testbed
+    # ----------------
+    if not runtime.testbed:
+        # If no testbed is provided, load the default one.
+        # Load default location of Testbed
+        testbedfile = os.path.join("tests", "testbed.yaml")
+        testbed = load(testbedfile)
+    else:
+        # Use the one provided
+        testbed = runtime.testbed
 
-    runtime.tasks.run(
-        testscript="tests/verify_ntp.py",
-        # testbed="testbed.yaml",
-        runtime=runtime,
-    )
+    # Find the location of the script in relation to the job file
+    testscript = os.path.join(os.path.dirname(__file__), "verify_ntp.py")
+
+    # run script
+    runtime.tasks.run(testscript=testscript, testbed=testbed)
