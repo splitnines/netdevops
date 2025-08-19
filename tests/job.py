906825ae79f125b0e350a0c1eb92eab03822400a
splitnines <splitnines@gmail.com>
2025-08-18 19:28:02 -0700

    Testing123

--- tests/job.py
+++ tests/job.py
@@ -4,4 +4,8 @@ from pyats.easypy import run
 def main(runtime):
     runtime.job.name = "verify_ntp.py"
 
-    run(testscript="verify_ntp.py", runtime=runtime)
+    run(
+        testscript="verify_ntp.py",
+        testbed="../testbed.yaml",
+        runtime=runtime,
+    )
