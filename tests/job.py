a809437dc0f44d36ae1cb56e97e4958a31c7986a
Rickey Ingrassia <splitnines@gmail.com>
2026-07-17 17:28:37 -0700

    cleaned up job.py

--- tests/job.py
+++ tests/job.py
@@ -3,20 +3,12 @@ from genie.testbed import load
 
 
 def main(runtime):
-    # ----------------
-    # Load the testbed
-    # ----------------
     if not runtime.testbed:
-        # If no testbed is provided, load the default one.
-        # Load default location of Testbed
         testbedfile = os.path.join("tests", "testbed.yaml")
         testbed = load(testbedfile)
     else:
-        # Use the one provided
         testbed = runtime.testbed
 
-    # Find the location of the script in relation to the job file
     testscript = os.path.join(os.path.dirname(__file__), "verify_ntp.py")
 
-    # run script
     runtime.tasks.run(testscript=testscript, testbed=testbed)
