279c3b7a5df9b9f190bed19f6768d018b21128cf
Rickey Ingrassia <splitnines@gmail.com>
2026-07-31 09:14:59 -0700

    building and testing the pipeline at the same time

--- local-pipeline/test.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-logger() {
-    local prefix=${1:-cicd}
-    ts=$(date '+%Y-%m-%d %H:%M:%S')
-    printf '%s [%s-commit]:' "$ts" "$prefix"
-}
-
-echo "$(logger "post") testing..."
